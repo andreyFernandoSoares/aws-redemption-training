984bd45192ee8764dcb879b62c3532ac26aa6332
andreyFernadoSoares <67378685+andreyFernandoSoares@users.noreply.github.com>
2022-04-25 11:20:38 -0300

    Update create-movie-lambda.py

--- create-movie-lambda.py
+++ create-movie-lambda.py
@@ -1,10 +1,15 @@
 import boto3
+import uuid
 
 client = boto3.resource('dynamodb')
 
 def lambda_handler(event, context):
+    movieId = srt(uuid.uuid4())
+    event['movieId'] = movieId
+    
     table = client.Table("movies")
     table.put_item(Item=event)
+    
     return {
         "status": 201,
         "mensagem": "Filme criado com sucesso"
