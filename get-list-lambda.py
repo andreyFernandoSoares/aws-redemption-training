d2b71bc1920e237e45da6c60f181da803b19fe58
andreyFernadoSoares <67378685+andreyFernandoSoares@users.noreply.github.com>
2022-05-23 19:41:30 -0300

    Update get-list-lambda.py

--- get-list-lambda.py
+++ get-list-lambda.py
@@ -3,7 +3,6 @@ import boto3
 client = boto3.resource('dynamodb')
 
 def lambda_handler(event, context):
-    userId = event['pathParameters']['userid']
     table = client.Table("movie")
     response = table.scan()
     data = response['Items']
